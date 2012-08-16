a619eb038a121d0bb372a23af3abd82bfbb33592
Hamatti <jumasan@utu.fi>
2012-08-16 10:24:52 +0200

    Changed from grep to egrep and made it oneliner

--- day_grep.sh
+++ day_grep.sh
@@ -5,9 +5,6 @@ channel=$2
 file="#$channel.log"
 path="" # Add path to the log folder
 
-grep -n $query "$path$file" > matches.log
-grep -n "Day changed" "$path$file" >> matches.log
-
-cat matches.log | sort -n |cut -d: -f2,3,4,5 |grep -B1 "^[^-]" |sed '/^--$/d'
+egrep "$query|Day changed" "$path$file" |grep -B1 "^[^-]" |sed '/^--$/d'
 
 
