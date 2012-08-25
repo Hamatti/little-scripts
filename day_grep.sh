8b02342c2a00830b6defc261835eea21dfca04e8
Hamatti <jumasan@utu.fi>
2012-08-25 11:30:35 +0200

    -i for incasesensitivity

--- day_grep.sh
+++ day_grep.sh
@@ -8,6 +8,6 @@ channel=$2
 file="#$channel.log"
 path="" # Add path to the log folder
 
-egrep "$query|Day changed" "$path$file" |grep -B1 "^[^-]" |sed '/^--$/d'
+egrep -i "$query|Day changed" "$path$file" |grep -B1 "^[^-]" |sed '/^--$/d'
 
 
